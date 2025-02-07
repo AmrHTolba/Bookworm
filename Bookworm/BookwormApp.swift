6ae3d0b2fac34767603e84893ed5797d6fb17f81
AmrFiqi <amr.h.elfiqi@gmail.com>
2025-02-07 16:08:27 +0200

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Amr El-Fiqi on 07/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
