93835e1a4bedb9929ba8f9b3811e6ec4f8ed69c6
AmrFiqi <amr.h.elfiqi@gmail.com>
2025-02-07 16:49:39 +0200

    Created AddBookView for adding new books

---
+++ Bookworm/View/AddBookView.swift
@@ -0,0 +1,25 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by Amr El-Fiqi on 07/02/2025.
+//
+
+import SwiftData
+import SwiftUI
+
+struct AddBookView: View {
+    // MARK: - Properties
+    @Environment(\.modelContext) var modelContext
+    
+    
+    
+    // MARK: - Body
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddBookView()
+}
