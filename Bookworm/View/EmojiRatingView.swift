53c9d8001a35ab86509c889ef7d3388ed9c7148b
AmrFiqi <amr.h.elfiqi@gmail.com>
2025-02-10 14:47:57 +0200

    Created EmojiRatingView
    returns emoji based on selected rating

---
+++ Bookworm/View/EmojiRatingView.swift
@@ -0,0 +1,32 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Amr El-Fiqi on 10/02/2025.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating: Int
+    
+    var body: some View {
+        switch rating {
+        case 1:
+            Text("😒")
+        case 2:
+            Text("😕")
+        case 3:
+            Text("😐")
+            case 4:
+            Text("😍")
+        default:
+            Text("🤯")
+        }
+        
+    }
+}
+
+#Preview {
+    EmojiRatingView(rating: 3)
+}
